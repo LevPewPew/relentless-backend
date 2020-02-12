a5cc48b550604b61037f5fb1b36b989019c77938
LevPewPew <levthedev@protonmail.com>
2020-02-12 21:02:27 +1100

    fixed syntax error with cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.DB_URL;
 const dbConfig = { useNewUrlParser: true, useUnifiedTopology: true };
-const whitelist = ['http://localhost:3000', 'https://sushi-dev-portal.netlify.com']
+const whitelist = ['http://localhost:3000', 'https://sushi-dev-portal.netlify.com'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -18,7 +18,7 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   }
-}
+};
 
 mongoose.connect(DB_URL, dbConfig, (err) => {
   if(err) {
@@ -30,7 +30,7 @@ mongoose.connect(DB_URL, dbConfig, (err) => {
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors({corsOptions}));
+app.use(cors(corsOptions));
 app.use(require('./routes/index'));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
