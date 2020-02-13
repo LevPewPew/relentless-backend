6bbaee4f4b46b4979e7aa5fda72fd4bea85a7c2f
LevPewPew <levthedev@protonmail.com>
2020-02-13 14:26:42 +1100

    tweaks

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.DB_URL;
 const dbConfig = { useNewUrlParser: true, useUnifiedTopology: true };
-const whitelist = ['http://localhost:3000', 'https://sushi-dev-portal.netlify.com'];
+const whitelist = ['http://localhost:3000', 'https://FRONT-END-URL-PLACEHOLDER']; // REPLACE
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const index = (req, res) => {
 
 router.get('/', index);
 
-// define routes here, this will mean anything in the sushi-routes.js file (yet to be created) will assume that /sushi will prepend the route path defined in sushi-routes.js
+// define routes here, this will mean anything in the categories-routes.js file (yet to be created) will assume that /categories will prepend the route path defined in categories-routes.js
 router.use('/categories', require('./categories-routes'));
 
 module.exports = router;
